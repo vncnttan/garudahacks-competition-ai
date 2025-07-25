a89b830a086fa71e7311481865fad4a03324a603
vncnttan <vincent.tanjaya.vt@gmail.com>
2025-07-25 14:53:38 +0700

    feat: AI search language source

--- RAG_powered_search/rag_powered_search.py
+++ RAG_powered_search/rag_powered_search.py
@@ -4,6 +4,9 @@ from RAG_powered_search.utils import handle_user_query
 from RAG_powered_search.import_postgresql_to_milvus import get_embedding
 from langchain_milvus import Milvus
 import os
+from constants import translation_lang
+import httpx
+from urllib.parse import quote
 import cohere
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
@@ -31,13 +34,29 @@ def llm_answer(query: str, language_src: str, language_dst: str = "id") -> str:
 
 co = cohere.Client()
 
-def llm_search_from_milvus(query: str, language_dst: str) -> str:
+async def llm_search_from_milvus(query: str, language_dst: str, language_src: str = "id") -> str:
     search_kwargs = {"k": 5}
     if language_dst:
         search_kwargs["filter"] = {"language_code": language_dst}
 
+    if language_src != "id":
+        try:
+            encoded_text = quote(query)
+            sl_code = translation_lang.get(language_src, {}).get('sl_code', 'id')
+            url = f"https://ftapi.pythonanywhere.com/translate?sl={sl_code}&dl=id&text={encoded_text}"
 
-    print("Search KWARGS:", search_kwargs)
+            async with httpx.AsyncClient() as client:
+                translation_api_response = await client.get(url, timeout=20.0)
+                translation_api_response.raise_for_status()
+                data = translation_api_response.json()
+                query = data.get('destination-text', 'Translation failed.')
+
+        except httpx.RequestError as exc:
+            print(f"An error occurred while requesting translation: {exc}")
+        except Exception as e:
+            print(f"An error occurred during translation processing: {e}")
+
+    print("Search Query:", query)
     results = vector_store.similarity_search(
         query=query,
         **search_kwargs
